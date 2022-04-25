128b607642bcea32b47a6fb258abec0676d6ef45
Henrique Moises Fernandes <henriqemoises@gmail.com>
2022-04-25 18:06:01 +0100

    Added solution to part 2 of year 2015 day 1.

--- Henrique/AdventOfCode/Code/2015/Problem_2015_1.cs
+++ Henrique/AdventOfCode/Code/2015/Problem_2015_1.cs
@@ -8,14 +8,46 @@
 
         internal override string Solve()
         {
+            string part1 = SolvePart1();
+            string part2 = SolvePart2();
 
+            return $"Part 1 solution: " + part1 + "\n" 
+                + "Part 2 solution: " + part2;
+
+
+
+
+        }
+
+        private string SolvePart1()
+        {
             int upStairs, downStaris = 0;
 
             upStairs = Input.Count(inpt => inpt == '(');
             downStaris = Input.Count(inpt => inpt == ')');
 
             return (upStairs - downStaris).ToString();
+        }
 
+        private string SolvePart2()
+        {
+            int level = 0;
+            for(int i = 0; i < Input.Length; i++)
+            {
+                if (Input[i] == '(')
+                {
+                    level++;
+                }
+                if (Input[i] == ')')
+                {
+                    level--;
+                }
+                if(level < 0)
+                {
+                    return (i + 1).ToString();
+                }
+            }
+            return "Solution not found.";
         }
 
     }
