5dd82bc9db8abf01a1ff69762b27b85d43eb00f4
Ana Isabel Galvão <anaisabel93@outlook.pt>
2022-05-01 22:47:04 +0100

    Added Ana's Days one and two 2015

---
+++ Ana/AdventOfCode/src/pt/ana/adventofcode/ConsoleApplication.java
@@ -0,0 +1,11 @@
+package pt.ana.adventofcode;
+import java.io.Console;
+
+class ConsoleApplication{    
+public static void main(String args[]){    
+Console c=System.console();    
+System.out.println("Enter your name: ");    
+String n=c.readLine();    
+System.out.println("Welcome "+n);    
+}    
+}  
