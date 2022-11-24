735a798e40c6101201250a6ba7291abf24b64227
Henrique Moises Fernandes <henriqemoises@gmail.com>
2022-11-24 16:46:26 +0000

    Added initial structure for problem 17 from year 2015.

---
+++ Henrique/AdventOfCode/Code/2015/Problem_2015_17.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Helpers;
+
+namespace AdventOfCode.Code
+{
+    internal class Problem_2015_17 : Problem
+    {
+        
+
+        internal Problem_2015_17() : base()
+        { }
+
+        internal override string Solve()
+        {
+            List<int> ContainerSizes = new List<int>();
+            try
+            {
+                ContainerSizes.AddRange(InputLines.Select(line => int.Parse(line)));
+            }
+            catch
+            {
+                throw new Exception("Invalid line in input.");
+            }
+
+            var sets = SetsGenerator.GenerateAllSets(InputLines.Count(), ContainerSizes);
+
+            string part1 = SolvePart1();
+            string part2 = SolvePart2();
+
+            return $"Part 1 solution: " + part1 + "\n"
+                + "Part 2 solution: " + part2;
+        }
+
+        private string SolvePart1()
+        {
+            return "";
+        }
+
+        private string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
---
+++ Henrique/AdventOfCode/Helpers/SetsGenerator.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode.Helpers
+{
+    internal static class SetsGenerator
+    {
+
+        /// <summary>
+        /// Generate all the subsets of size subsetSize from the valuesList list.
+        /// </summary>
+        /// <param name="subsetSize"></param>
+        /// <param name="valuesList"></param>
+        /// <returns>A list with all subsets of size subsetSize of the valuesList set.</returns>
+        internal static IEnumerable<IEnumerable<int>> GenerateSets(int subsetSize, List<int> valuesList)
+        {
+            List<List<int>> results = new List<List<int>>();
+            List<Tuple<List<int>, List<int>>> tempResults = new List<Tuple<List<int>, List<int>>>();
+
+            for (int i = 0; i < valuesList.Count; i++)
+            {
+                tempResults.Add(Tuple.Create(new List<int> { valuesList[i] }, valuesList.Skip(i + 1).ToList()));
+            }
+
+            int iteration = 1;
+            while (iteration < subsetSize)
+            {
+                List<Tuple<List<int>, List<int>>> iterationResults = new List<Tuple<List<int>, List<int>>>();
+                foreach (var elem in tempResults)
+                {
+                    if (elem.Item2.Any())
+                    {
+                        for (int i = 0; i < elem.Item2.Count(); i++)
+                        {
+                            List<int> newList = new List<int>(elem.Item1);
+                            List<int> newRemainderList = new List<int>(elem.Item2);
+
+                            newList.Add(elem.Item2[i]);
+                            newRemainderList.RemoveRange(0, i + 1);
+
+                            var newTuple = Tuple.Create(newList, newRemainderList);
+
+                            iterationResults.Add(newTuple);
+                        }
+                    }
+                }
+                iteration++;
+                tempResults = iterationResults;
+            }
+
+            results = tempResults.Select(r => r.Item1).ToList();
+            return results;
+        }
+
+        internal static IEnumerable<IEnumerable<int>> GenerateAllSets(int maxSubsetSize, List<int> valuesList)
+        {
+            List<IEnumerable<int>> sets = new List<IEnumerable<int>>();
+            for(int i = 1; i <= maxSubsetSize; i++)
+            {
+                sets.AddRange(GenerateSets(i, valuesList));
+            }
+            return sets;
+        }
+    }
+}
