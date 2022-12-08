51da4d879353c8631ce745c45d207db764ba0e02
Henrique Moisés Fernandes <henriqemoises@gmail.com>
2022-12-08 17:02:51 +0000

    Added solution for part 1 of day 19 from year 2015. Started implementation of part 2.

---
+++ Henrique/AdventOfCode/Code/2015/Entities/2015_19/Transformation.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Code._2015.Entities._2015_19
+{
+    internal class Transformation
+    {
+        internal string InitialMolecule { get; set; }
+        internal string FinalMolecule { get; set; }
+
+        internal Transformation(string initialMolecule, string finalMolecule)
+        {
+            InitialMolecule = initialMolecule;
+            FinalMolecule = finalMolecule;
+        }
+
+    }
+}
