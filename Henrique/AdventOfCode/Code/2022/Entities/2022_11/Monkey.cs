d3bf8687ee43b415b839814030b13ad7d951b3bb
Henrique Moisés Fernandes <henriqemoises@gmail.com>
2025-02-24 17:54:41 +0000

    Refactorização

--- Henrique/AdventOfCode/Code/2022/Entities/2022_11/Monkey.cs
+++ Henrique/AdventOfCode/Code/2022/Entities/2022_11/Monkey.cs
@@ -1,74 +1,40 @@
-﻿using AdventOfCode._2015_7;
-using AdventOfCode.Helpers;
-
-namespace AdventOfCode.Code._2022.Entities._2022_11
+﻿namespace AdventOfCode.Code._2022.Entities._2022_11
 {
-    internal class Monkey : ICloneable
+    internal class Monkey
     {
         internal int Number { get; set; }
-        internal List<Tuple<int, string, int, List<int>>> CurrentItems { get; set; } = [];
-        internal List<Tuple<int, string, int, List<int>>> InitialItems { get; set; } = [];
-
-        internal Func<string, string> Operation { get; set; }
-        internal Func<string, bool> Test { get; set; }
+        internal List<long> CurrentItems { get; set; } = [];
+        internal Func<long, long> Operation { get; set; }
+        internal Func<long, bool> Test { get; set; }
         internal int ReceiverMonkeyIfTestTrue { get; set; }
         internal int ReceiverMonkeyIfTestFalse { get; set; }
         internal int InspectionsCount { get; set; }
-        internal int TestValue { get; set; }
 
-        internal Monkey(int number, List<Tuple<int, string, List<int>>> initialItems, char operation, string operationValue, int testValue, int receiverMonkeyIfTestTrue, int receiverMonkeyIfTestFalse)
+        internal Monkey(int number, List<long> initialItems, char operation, string operationValue, int testValue, int receiverMonkeyIfTestTrue, int receiverMonkeyIfTestFalse)
         {
             Number = number;
-            CurrentItems = initialItems.Select(x => new Tuple <int, string, int, List<int>>(x.Item1, x.Item2, int.Parse(x.Item2), x.Item3)).ToList();
-            InitialItems = initialItems.Select(x => new Tuple<int, string, int, List<int>>(x.Item1, x.Item2, int.Parse(x.Item2), x.Item3)).ToList();
+            CurrentItems = initialItems;
             Operation = operation switch
             {
-                '+' => (value) => MathOperations.SumStrings(value.ToString(), (operationValue == "old" ? value.ToString() : operationValue)),
-                '*' => (value) => MathOperations.MultiplyStrings(value.ToString(), (operationValue == "old" ? value.ToString() : operationValue)),
+                '+' => (value) => value + (operationValue == "old" ? value : long.Parse(operationValue)),
+                '*' => (value) => value * (operationValue == "old" ? value : long.Parse(operationValue)),
                 _ => throw new Exception("Invalid line in input.")
             };
-            TestValue = testValue;
-            Test = (value) => MathOperations.ModuloOfString(value, testValue) == 0;
+            Test = (value) => value % testValue == 0;
             ReceiverMonkeyIfTestTrue = receiverMonkeyIfTestTrue;
             ReceiverMonkeyIfTestFalse = receiverMonkeyIfTestFalse;
         }
 
-        private Monkey(int number, List<Tuple<int, string, int, List<int>>> initialItems, Func<string, string> operation, Func<string, bool> test, int receiverMonkeyIfTestTrue, int receiverMonkeyIfTestFalse)
-        {
-            Number = number;
-            CurrentItems = initialItems.Select(x => new Tuple<int, string, int, List<int>>(x.Item1, x.Item2, x.Item3, x.Item4)).ToList();
-            Operation = operation;
-            Test = test;
-            ReceiverMonkeyIfTestTrue = receiverMonkeyIfTestTrue;
-            ReceiverMonkeyIfTestFalse = receiverMonkeyIfTestFalse;
-        }
-
-        internal string ApplyOperation(string value)
+        internal long ApplyOperation(long value)
         {
             value = Operation(value);
             return value;
         }
 
-        internal double DecreaseWorryLevelByDivision(double value)
+        internal long DecreaseWorryLevelByDivision(long value)
         {
-            value = (double)Math.Floor((decimal)value / 3);
+            value = (long)Math.Floor((decimal)value / 3);
             return value;
-        }
-
-        internal Tuple<int, string, int, List<int>> DecreaseWorryLevelByLookup(Tuple<int, string, int, List<int>> newItem)
-        {
-            if (MathOperations.ModuloOfString(newItem.Item2, newItem.Item3) == 0 && MathOperations.ModuloOfString(newItem.Item2, TestValue) == 0)
-            {
-                newItem = new Tuple<int, string, int, List<int>>(newItem.Item1, newItem.Item3.ToString(), newItem.Item3, newItem.Item4);
-            }
-            return newItem;
-        }
-
-        public object Clone()
-        {
-            Monkey cloneMonkey = new Monkey(Number, new List<Tuple<int, string, int, List<int>>>(CurrentItems), Operation, Test, ReceiverMonkeyIfTestTrue, ReceiverMonkeyIfTestFalse);
-            cloneMonkey.TestValue = TestValue;
-            return cloneMonkey;
         }
     }
 }
--- Henrique/AdventOfCode/Code/2022/Problem_2022_11.cs
+++ Henrique/AdventOfCode/Code/2022/Problem_2022_11.cs
@@ -1,7 +1,5 @@
-﻿using AdventOfCode._2015_21;
-using AdventOfCode.Code._2022.Entities._2022_11;
+﻿using AdventOfCode.Code._2022.Entities._2022_11;
 using System.Text.RegularExpressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdventOfCode.Code
 {
@@ -23,15 +21,15 @@ namespace AdventOfCode.Code
         public override string Solve()
         {
             List<Monkey> monkeys = [];
+            List<Monkey> monkeyClones = [];
             int currentMonkey = 0;
-            List<Tuple<int, string, List<int>>> currentMonkeyInitialItems = [];
+            List<long> currentMonkeyInitialItems = [];
             char currentMonkeyOperation = new();
             string currentMonkeyOperationValue = "";
             int currentMonkeyTest = 0;
             string currentMonkeyConditionValue = "";
             int currentMonkeyConditionReceiverMonkeyTrue = 0;
             int currentMonkeyConditionReceiverMonkeyFalse = 0;
-            int itemNumber = 0;
 
             foreach (string line in InputLines)
             {
@@ -40,6 +38,7 @@ namespace AdventOfCode.Code
                     if (string.IsNullOrWhiteSpace(line))
                     {
                         monkeys.Add(new Monkey(currentMonkey, currentMonkeyInitialItems, currentMonkeyOperation, currentMonkeyOperationValue, currentMonkeyTest, currentMonkeyConditionReceiverMonkeyTrue, currentMonkeyConditionReceiverMonkeyFalse));
+                        monkeyClones.Add(new Monkey(currentMonkey, currentMonkeyInitialItems, currentMonkeyOperation, currentMonkeyOperationValue, currentMonkeyTest, currentMonkeyConditionReceiverMonkeyTrue, currentMonkeyConditionReceiverMonkeyFalse));
 
                         currentMonkeyInitialItems = [];
                         currentMonkeyOperation = new();
@@ -65,10 +64,9 @@ namespace AdventOfCode.Code
                     match = startingItemsRegexPattern.Match(line);
                     if (match.Success)
                     {
-                        foreach (string value in match.Groups["value"].Captures.Select(x => x.Value))
+                        foreach (long value in match.Groups["value"].Captures.Select(x => long.Parse(x.Value)))
                         {
-                            currentMonkeyInitialItems.Add(new Tuple<int, string, List<int>>(itemNumber, value, []));
-                            itemNumber++;
+                            currentMonkeyInitialItems.Add(value);
                         }
                     }
 
@@ -105,9 +103,8 @@ namespace AdventOfCode.Code
                 }
             }
 
-            List<Monkey> monkeyListClone = monkeys.ConvertAll(m => (Monkey)m.Clone());
-            string part1 = "";// SolvePart1(monkeys);
-            string part2 = SolvePart2(monkeyListClone);
+            string part1 = SolvePart1(monkeys);
+            string part2 = SolvePart2(monkeyClones);
 
             return $"Part 1 solution: {part1}\nPart 2 solution: {part2}";
 
@@ -134,31 +131,22 @@ namespace AdventOfCode.Code
                     for (int j = 0; j < monkeyTotalItems; j++)
                     {
                         monkey.InspectionsCount++;
-                        string itemBeforeOperation = monkey.CurrentItems.First().Item2;
-                        Tuple<int, string, int, List<int>> newItem;
-                        string newItemValue;
-                        if (!decreaseWorryLevelAfterOperation)
+                        long itemBeforeOperation = monkey.CurrentItems.First();
+                        long newItemValue;
+                        newItemValue = monkey.ApplyOperation(monkey.CurrentItems.First());
+                        if (decreaseWorryLevelAfterOperation)
                         {
-                            newItem = new Tuple<int, string, int, List<int>>(monkey.CurrentItems[0].Item1, monkey.CurrentItems[0].Item2, monkey.CurrentItems[0].Item3, monkey.CurrentItems[0].Item4);
-                            newItemValue = monkey.ApplyOperation(monkey.CurrentItems[0].Item2);
-                            newItem = new Tuple<int, string,int, List<int>>(monkey.CurrentItems[0].Item1, newItemValue, monkey.CurrentItems[0].Item3, monkey.CurrentItems[0].Item4);
-                        }
-                        else
-                        {
-                            newItemValue = monkey.ApplyOperation(monkey.CurrentItems.First().Item2);
-                            newItemValue = monkey.DecreaseWorryLevelByDivision(double.Parse(newItemValue)).ToString();
-                            newItem = new Tuple<int, string,int, List<int>>(monkey.CurrentItems[0].Item1, newItemValue, monkey.CurrentItems[0].Item3, []);
+                            newItemValue = monkey.DecreaseWorryLevelByDivision(newItemValue);
                         }
 
                         monkey.CurrentItems.RemoveAt(0);
-                        newItem.Item4.Add(monkey.Number);
                         if (monkey.Test(newItemValue))
                         {
-                            monkeys.Single(m => m.Number == monkey.ReceiverMonkeyIfTestTrue).CurrentItems.Add(newItem);
+                            monkeys.Single(m => m.Number == monkey.ReceiverMonkeyIfTestTrue).CurrentItems.Add(newItemValue);
                         }
                         else
                         {
-                            monkeys.Single(m => m.Number == monkey.ReceiverMonkeyIfTestFalse).CurrentItems.Add(newItem);
+                            monkeys.Single(m => m.Number == monkey.ReceiverMonkeyIfTestFalse).CurrentItems.Add(newItemValue);
                         }
                     }
                 }
@@ -166,15 +154,14 @@ namespace AdventOfCode.Code
                 foreach (Monkey monkey in monkeys)
                 {
                     List<Tuple<int, string, int, List<int>>> newValues = [];
-                    for(int j = 0; j < monkey.CurrentItems.Count; j++ )
+                    for (int j = 0; j < monkey.CurrentItems.Count; j++)
                     {
                         var item = monkey.CurrentItems[j];
-                        double itemValue = double.Parse(item.Item2) % 9699690;
-                        item = new Tuple<int, string, int, List<int>>(item.Item1, itemValue.ToString(), item.Item3, item.Item4);
-                        monkey.CurrentItems[j] = item;
+                        long itemValue = item % 9699690;
+                        monkey.CurrentItems[j] = itemValue;
                     }
                 }
-            }            
+            }
 
             IEnumerable<Monkey> mostActiveMonkeys = monkeys.OrderByDescending(m => m.InspectionsCount).Take(2);
             long monkeyBusinessValue = 1;
